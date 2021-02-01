d2761da142b74b9bed9bd748c32b01ab8c9437a4
aayush.bhankale <aayush.bhankale@scikey.ai>
2021-02-01 21:55:30 +0530

    chore: reactive forms

--- src/app/components/dashboard/forms/reactive-forms/reactive-forms.component.ts
+++ src/app/components/dashboard/forms/reactive-forms/reactive-forms.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AsyncValidatorFn, FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-reactive-forms',
@@ -8,19 +9,55 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class ReactiveFormsComponent implements OnInit {
   myReactiveForm!: FormGroup;
+  notAllowedNames = ['avb786', 'yash_56']
+  formBodyData: any = 'pending';
   constructor() { }
 
   ngOnInit(): void {
     this.myReactiveForm = new FormGroup({
-      'userDetails':  new FormGroup({
-        'username': new FormControl(null, Validators.required),
-        'email': new FormControl(null, [Validators.email, Validators.required])
+      'userDetails': new FormGroup({
+        'username': new FormControl(null, [Validators.required, this.notAllowedNameData.bind(this)]),
+        'email': new FormControl(null, [Validators.email, Validators.required], this.notAllowedEmailData)
       }),
       'course': new FormControl('vue'),
-      'gender': new FormControl('male')
+      'gender': new FormControl('male'),
+      'skills': new FormArray([
+        new FormControl(null)
+      ])
     })
   }
-  onSubmit(){
+  onSubmit() {
     console.log(this.myReactiveForm)
+    if(this.myReactiveForm.valid) {
+      this.formBodyData = this.myReactiveForm.value;;
+    } else {
+      this.formBodyData = "Invalid";
+    }
   }
+  onAddSkills() {
+    const controlsForSkills = new FormControl(null, Validators.required);
+    (<FormArray>this.myReactiveForm.get('skills')).push(controlsForSkills)
+  }
+  notAllowedNameData(control: FormControl) {
+    if (this.notAllowedNames.indexOf(control.value) !== -1) {
+      return {
+        'nameNotAllowed': true
+      }
+    }
+    return null;
+  }
+ private notAllowedEmailData(control: FormControl) : Promise<any> | Observable<any> | AsyncValidatorFn {
+   const  myRes = new Promise((resolve, reject) => {
+     setTimeout(() => {
+      if(control.value === 'avb@gmail.com') {
+        resolve({'emailNotAllowed': true})
+      } else {
+        resolve(null)
+      }
+     }, 2500);
+   })
+   return myRes;
+}
+
+
 }

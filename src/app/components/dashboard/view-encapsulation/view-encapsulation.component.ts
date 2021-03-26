80ca2754e02efcfb3e43eb61950015575d821671
Aayush Bhankale <62827799+avb786@users.noreply.github.com>
2021-03-26 12:25:14 +0530

    Update view-encapsulation.component.ts

--- src/app/components/dashboard/view-encapsulation/view-encapsulation.component.ts
+++ src/app/components/dashboard/view-encapsulation/view-encapsulation.component.ts
@@ -12,5 +12,32 @@ export class ViewEncapsulationComponent implements OnInit {
 
   ngOnInit(): void {
   }
+    // async selectFile(e) {
+  //   console.log("===", e.target.files);
+  //   this.files = e.target.files;
+  //   this.urls ='';
+  //   for (let i = 0; i < this.files.length; i++) {
+  //     let reader = new FileReader();
+  //     reader.onload = (e: any) => {
+  //       const obj = {
+  //         url: e.target.result,
+  //         imageName: this.files[i].name,
+  //       }
+  //       if (this.files[i].type == 'image/png' || this.files[i].type == 'image/jpeg' || this.files[i].type == 'image/jpg') {
+  //         obj['type'] = 'image'
+  //       } else {
+  //         let type = this.files[i].name.split('.');
+  //         obj['type'] = type[type.length - 1]
+  //         console.log("type", type);
+  //       }
+  //       console.log("OBJ", encode(obj.url));
+  //       this.showImage = obj.url;
+  //       this.showImageName = obj.imageName;
+  //       this.urls = encode(obj.url)
+        
+  //     }
+  //     await reader.readAsDataURL(this.files[i]);
+  //   }
+  // }
 
 }
